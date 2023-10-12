2a9c9d94b57988ae420e62862ae51a276c393e6c
timoteifrincuu <146390788+timoteifrincuu@users.noreply.github.com>
2023-10-12 22:35:17 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace impartireanr
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y, z;
+
+            x = Convert.ToInt32(Console.ReadLine());
+            y = Convert.ToInt32(Console.ReadLine());
+            z = x / y;
+           
+            if (y != 0 && x % y == 0)
+            {
+                Console.WriteLine(z);
+            }
+            else
+            {
+                Console.WriteLine("Impartirea nu poate fi efectuata");
+            }
+        }
+    }
+}
